381223e9e9f6ddb50b64676f848d1a48e14b544d
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-06-10 19:35:43 +0000

    Finalized up to 25

--- lib/pokedex_cli_gem/cli.rb
+++ lib/pokedex_cli_gem/cli.rb
@@ -45,13 +45,13 @@ class CommandLineInterface
 		puts ""
 		puts "What".colorize(:color => :blue, :mode => :bold)+ " " + "Pokemon".colorize(:color => :yellow, :mode => :bold) + " " + "number would you like to see?".colorize(:color => :blue, :mode => :bold)
 
-    puts "Enter a number from 1 - 809 to see 50".colorize(:color => :blue, :mode => :bold) + " " + "Pokemon".colorize(:color => :yellow, :mode => :bold) + " " + "from the number you've entered.".colorize(:color => :blue, :mode => :bold)
+    puts "Enter a number from 1 - 809 to see 25".colorize(:color => :blue, :mode => :bold) + " " + "Pokemon".colorize(:color => :yellow, :mode => :bold) + " " + "from the number you've entered.".colorize(:color => :blue, :mode => :bold)
 		from_number = gets.strip.to_i
 		puts ""
-    puts "-----------------------".colorize(:color => :red, :mode => :bold) + " Pokemon".colorize(:color => :yellow, :mode => :bold) + " #{from_number} - #{from_number+49} -----------------------".colorize(:color => :red, :mode => :bold)
+    puts "-----------------------".colorize(:color => :red, :mode => :bold) + " Pokemon".colorize(:color => :yellow, :mode => :bold) + " #{from_number} - #{from_number+24} -----------------------".colorize(:color => :red, :mode => :bold)
     puts ""
     puts "--------------------------------------------------------------".colorize(:color => :red, :mode => :bold)
-    Pokemon.ordered_list[from_number-1,50].each.with_index(from_number) do |pokemon, index|
+    Pokemon.ordered_list[from_number-1,25].each.with_index(from_number) do |pokemon, index|
       puts "#{index}.".colorize(:color => :yellow,:mode => :bold) + " " + "#{pokemon.name}".colorize(:color => :blue, :mode => :bold)
     end
   end
